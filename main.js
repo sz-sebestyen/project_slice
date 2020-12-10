b8d66b8815666243c014846a4bae495cfd68fa0c
sz-sebestyen <szabosebestyen.mail@gmail.com>
2020-12-10 09:29:57 +0100

    Add TODO comments

--- main.js
+++ main.js
@@ -7,7 +7,27 @@
 'use strict';
 
 function _load() {
+	// root elem tárolsa
 	const root = document.getElementById("root");
+
+
+	// üres mezők száma
+	let n = 10;
+
+
+	// html strucktúra egy változóban
+	// div checkbox,textinput,törlésbutton
+
+
+	// n-szer duplikálni, és hozzáadni a root-hoz
+
+
+	// root elemen kívüre andni egy hozzáadás gombot
+	// addeventlitener click, újabb sort adunk a roothoz
+
+
+	// kijelölöm classnév alapján az össze törlás gombot > változóba
+	// forciklusban addEventListener click
 
 
 }
