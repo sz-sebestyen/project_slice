30ce7bc48bc550a60fc3be12dd1bafe8c3213e2d
sz-sebestyen <szabosebestyen.mail@gmail.com>
2020-12-10 09:12:46 +0100

    Add description

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function _load() {}
+// Feladat: TODO aplikáció
+// 10 üres mező áll rendelkezésre alapból
+// hozzáadás és törlés gomb
+// checkboxok állapotjelzésre
+// text input mező leírásnak
+
+'use strict';
+
+function _load() {
+	const root = document.getElementById("root");
+
+
+}
 
 window.addEventListener("load", _load);
